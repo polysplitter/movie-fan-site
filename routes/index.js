ccacab9c3fe3fce38043cc60843b1b42a09fef7c
Kyle <polysplitter@gmail.com>
2018-12-29 16:41:11 -0600

    implemented a very poor search

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 let router = express.Router();
 const request = require('request')
 
+// https://www.themoviedb.org/documentation/api?language=en-US
 const apiKey = ''
 
 const apiBaseUrl = 'http://api.themoviedb.org/3';
@@ -35,6 +36,24 @@ router.get('/movie/:id', (req, res, next) => {
     const parsedData = JSON.parse(movieData)
     res.render('single-movie', {
       movie: parsedData
+    })
+  })
+})
+
+router.post('/search', (req, res, next) => {
+  const userSearchTerm = encodeURI(req.body.movieSearch)
+  const cat = req.body.cat
+  const movieUrl = `${apiBaseUrl}/search/${cat}?query=${userSearchTerm}&api_key=${apiKey}`
+  request.get(movieUrl, (error, response, movieData) => {
+    let parsedData = JSON.parse(movieData)
+
+    // lots of bugs around search.
+    if(cat == "person") {
+      parsedData.results = parsedData.results[0].known_for
+    }
+
+    res.render('index', {
+      parsedData: parsedData.results
     })
   })
 })
